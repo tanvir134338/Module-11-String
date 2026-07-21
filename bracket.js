acab95345e4ae66d8efc4eac45ac16943ccc5e80
tanvir134338 <tanvir134338@gmail.com>
2026-07-22 04:30:17 +0600

    dot, bracket

---
+++ bracket.js
@@ -0,0 +1,11 @@
+const person={
+    name:'Goru🐄',
+    profession:'Ghas Khawya',
+    age:'1 year',
+    isSingle:true,
+    "home address":'Bagan'
+}
+
+console.log(person["home address"])
+person["home address"]='Goil';
+console.log(person)
---
+++ dot.js
@@ -0,0 +1,14 @@
+const person={
+    name:'Goru🐄',
+    profession:'Ghas Khawya',
+    age:'1 year',
+    isSingle:true
+}
+// console.log(person);
+
+const age =person.age;
+person.age=2
+console.log(person.age)
+console.log(person);
+person.isSingle=false;
+console.log(person)
