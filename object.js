6537bf19f61589ca672400dd6990001c19c8ea63
tanvir134338 <tanvir134338@gmail.com>
2026-07-22 04:15:47 +0600

    object, reverse

---
+++ object.js
@@ -0,0 +1,16 @@
+// const cap = ['Khaki','CRR', 250, 200];
+
+/**
+ * name ; sunglass
+ * price : 250
+ * color: white
+ */
+
+const sunglass = {
+    color: 'White',
+    price: 250,
+    brand:'Ray Ban',
+    inStock:true,
+    variant:[250, 350, 450, 550, 950]
+};
+console.log(sunglass)
---
+++ reverse.js
@@ -0,0 +1,5 @@
+const comitment = 'I will work hard'
+const parts =comitment.split('');
+const revParts =parts.reverse();
+const join =revParts.join('');
+console.log(join);
